a3059f8423bae1d82159b06fd20bd4cf516c4b5d
man o war <rahibmemebook@gmail.com>
2025-03-11 00:28:43 +0600

    add up to string

---
+++ getting started/somthing.py
@@ -0,0 +1,8 @@
+class Logo:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def display(self):
+        print(f"Logo Name: {self.name}")
+        print(f"Logo Color: {self.color}")
---
+++ getting started/stringFuntion.py
@@ -0,0 +1,13 @@
+import string
+
+
+name = input("enter ur name: ")
+
+
+print(len(name))
+print(name.find(' '))
+print(name.rfind(' '))
+print(name.capitalize())
+print(name.isalpha())
+print(name.isdigit()) 
+print(name.replace('a','b'))
